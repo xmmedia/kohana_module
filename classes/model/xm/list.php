f11e4bdf86455d411c02697f4cb34387e278214f
Darryl Hein <dhein@xmmedia.com>
2010-12-19 23:39:55 -0700

    adding default list model that can be extended to save extra code

---
+++ classes/model/xm/list.php
@@ -0,0 +1,66 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+/**
+ * This model was created using cl4_ORM and should provide
+ * standard Kohana ORM features in additon to cl4-specific features.
+ */
+class Model_XM_List extends ORM {
+	protected $_table_names_plural = FALSE;
+
+	// column labels
+	protected $_labels = array(
+		'id' => 'ID',
+		'name' => 'Name',
+		'display_order' => 'Display Order',
+	);
+
+	// default sorting
+	protected $_sorting = array(
+		'display_order' => 'ASC',
+		'name' => 'ASC',
+	);
+
+	// Filters
+	protected $_filters = array(
+	    TRUE => array('trim' => array()),
+	);
+
+	// column definitions
+	protected $_table_columns = array(
+		/**
+		* see http://v3.kohanaphp.com/guide/api/Database_MySQL#list_columns for all possible column attributes
+		* see the modules/cl4/config/cl4orm.php for a full list of cl4-specific options and documentation on what the options do
+		*/
+		'id' => array(
+			'field_type' => 'hidden',
+			'edit_flag' => TRUE,
+			'display_order' => 10,
+			'is_nullable' => FALSE,
+		),
+		'name' => array(
+			'field_type' => 'text',
+			'list_flag' => TRUE,
+			'edit_flag' => TRUE,
+			'search_flag' => TRUE,
+			'view_flag' => TRUE,
+			'display_order' => 20,
+			'is_nullable' => FALSE,
+			'field_attributes' => array(
+				'maxlength' => 50,
+			),
+		),
+		'display_order' => array(
+			'field_type' => 'text',
+			'list_flag' => TRUE,
+			'edit_flag' => TRUE,
+			'search_flag' => TRUE,
+			'view_flag' => TRUE,
+			'display_order' => 30,
+			'is_nullable' => FALSE,
+			'field_attributes' => array(
+				'maxlength' => 6,
+				'size' => 6,
+			),
+		),
+	);
+} // class

5d56cb4682c282bf45d7cbc8a6dee9df2a859bb4
Darryl Hein <dhein@xmmedia.com>
2010-12-27 18:23:55 -0700

    adding ORM_HTMLBasic field type class so there is a still a class field type that is HTML but doesn't add a class automatically

---
+++ classes/orm/htmlbasic.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+class ORM_HTMLBasic extends XM_ORM_HTMLBasic {}
---
+++ classes/xm/orm/htmlbasic.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+class XM_ORM_HTMLBasic extends cl4_ORM_HTML {
+	/**
+	* Does the same thing as cl4_ORM_HTML::edit() but adds the class textarea_html to every input
+	*/
+	public static function edit($column_name, $html_name, $body, array $attributes = NULL, array $options = array(), ORM $orm_model = NULL) {
+		return cl4_ORM_HTML::edit($column_name, $html_name, $body, $attributes, $options, $orm_model);
+	}
+}

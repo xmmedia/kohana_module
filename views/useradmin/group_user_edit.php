f62ef20983700d24253c1202e29e861c16595b08
Darryl Hein <dhein@xmmedia.com>
2011-01-29 13:44:31 -0700

    view for group-user editing

---
+++ views/useradmin/group_user_edit.php
@@ -0,0 +1,43 @@
+<div class="group_perm_edit">
+
+<h1><?php echo HTML::chars($group->name); ?></h1>
+<?php if ( ! empty($group->description)) { ?><p>Description: <?php echo HTML::chars($group->description); ?></p><?php } ?>
+
+<?php echo Form::open(); ?>
+
+<div class="perm_available">
+	<h4>Users not in the group</h4>
+	<?php echo $available_users_select; ?>
+</div>
+<div class="perm_buttons">
+	<?php echo Form::input_button(NULL, 'Add »', array(
+		'class' => 'move_select_options',
+		'data-xm_from_select' => 'available_users[]',
+		'data-xm_to_select' => 'current_users[]',
+	)); ?>
+	<?php echo Form::input_button(NULL, '« Remove', array(
+		'class' => 'move_select_options',
+		'data-xm_from_select' => 'current_users[]',
+		'data-xm_to_select' => 'available_users[]',
+	)); ?>
+</div>
+<div class="perm_current">
+	<h4>Users in the group</h4>
+	<?php echo $current_users_select; ?>
+</div>
+<div class="clear"></div>
+
+<div class="cl4_buttons">
+	<?php echo Form::submit(NULL, 'Save', array('class' => 'permission_form_save')); ?>
+	<?php echo Form::input_button(NULL, 'Reset', array(
+		'class' => 'cl4_button_link',
+		'data-cl4_link' => URL::site(Request::instance()->uri()),
+	)); ?>
+	<?php echo Form::input_button(NULL, 'Cancel', array(
+		'class' => 'cl4_button_link',
+		'data-cl4_link' => URL::site(Route::get('useradmin')->uri(array('action' => 'cancel_group'))),
+	)); ?>
+</div>
+
+<?php echo Form::close(); ?>
+</div>

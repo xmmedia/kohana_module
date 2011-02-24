64be4c823d225729b4ab5a0bfc0d1d123ba36cf2
Darryl Hein <dhein@xmmedia.com>
2011-02-24 13:46:20 -0700

    removing config table from create sql as it's no longer used

--- sql/craete_mysql.sql
+++ sql/craete_mysql.sql
@@ -1,18 +1,5 @@
 -- Additional SQL to use all the features of the xmmedia module
 
--- Config table
-CREATE TABLE `config` (
-  `id` int(11) NOT NULL auto_increment,
-  `setting` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `name` varchar(50) collate utf8_unicode_ci NOT NULL,
-  `description` text collate utf8_unicode_ci NOT NULL,
-  `value` varchar(50) collate utf8_unicode_ci NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `setting` (`setting`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
-
 -- Contact table
 CREATE TABLE `contact` (
   `id` int(11) NOT NULL auto_increment,

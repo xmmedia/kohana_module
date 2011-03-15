7a95dfdd4baf7dd9c26b4a521022c725a8aed673
Darryl Hein <dhein@xmmedia.com>
2011-03-15 16:43:32 -0600

    tweaking account info email

--- views/useradmin/account_update_email.php
+++ views/useradmin/account_update_email.php
@@ -1,4 +1,4 @@
-<p>Your account <?php echo HTML::chars($app_name); ?> as been updated. Your new login information is as follows:</p>
+<p>Your <?php echo HTML::chars($app_name); ?> account as been updated. Your new login information is as follows:</p>
 <p>Username: <?php echo $username; ?><br>
 Password: <?php echo ($password === FALSE ? '<em>Your current password</em>' : $password); ?></p>
 <p>To login, <?php echo HTML::anchor($url, 'click here', array('target' => '_blank'), TRUE); ?> or copy and paste the following link into your browser:</p>

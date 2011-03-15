a05f816856dca469c2af23d951da147364be38bb
Darryl Hein <dhein@xmmedia.com>
2011-03-15 17:53:08 -0600

    tweaking and correcting the emails sent from user admin

--- views/useradmin/account_update_email.php
+++ views/useradmin/account_update_email.php
@@ -1,7 +1,7 @@
-<p>Your <?php echo HTML::chars($app_name); ?> account as been updated. Your new login information is as follows:</p>
+<p>Your <?php echo HTML::chars($app_name); ?> account has been updated. Your new login information is as follows:</p>
 <p>Username: <?php echo $username; ?><br>
 Password: <?php echo ($password === FALSE ? '<em>Your current password</em>' : $password); ?></p>
 <p>To login, <?php echo HTML::anchor($url, 'click here', array('target' => '_blank'), TRUE); ?> or copy and paste the following link into your browser:</p>
 <p><?php echo HTML::chars($url); ?></p>
-<p>If you have problems, please contact the administrator at <?php echo $support_email; ?>.</p>
+<p>If you any have problems, please don't hesitante to contact us at <?php echo $support_email; ?></p>
 <p>Thank you,<br><?php echo HTML::chars($app_name); ?></p>
--- views/useradmin/login_information_email.php
+++ views/useradmin/login_information_email.php
@@ -1,7 +1,7 @@
-<p>An administrator has reset your password on <?php echo HTML::chars($app_name); ?>. Your new login information is as follows:</p>
+<p>A <?php echo HTML::chars($app_name); ?> administrator has reset your password. Your new login information is as follows:</p>
 <p>Username: <?php echo $username; ?><br>
 Password: <?php echo $password; ?></p>
 <p>To login, <?php echo HTML::anchor($url, 'click here', array('target' => '_blank'), TRUE); ?> or copy and paste the following link into your browser:</p>
 <p><?php echo HTML::chars($url); ?></p>
-<p>If you have problems, please contact the administrator at <?php echo $support_email; ?>.</p>
+<p>If you any have problems, please don't hesitante to contact us at <?php echo $support_email; ?></p>
 <p>Thank you,<br><?php echo HTML::chars($app_name); ?></p>

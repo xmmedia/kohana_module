9ae2b3f662e946f89aafa19851fc526b29f0f622
Darryl Hein <dhein@xmmedia.com>
2011-03-16 14:19:46 -0600

    spelling correction

--- views/useradmin/login_information_email.php
+++ views/useradmin/login_information_email.php
@@ -3,5 +3,5 @@
 Password: <?php echo $password; ?></p>
 <p>To login, <?php echo HTML::anchor($url, 'click here', array('target' => '_blank'), TRUE); ?> or copy and paste the following link into your browser:</p>
 <p><?php echo HTML::chars($url); ?></p>
-<p>If you any have problems, please don't hesitante to contact us at <?php echo $support_email; ?></p>
+<p>If you have any problems, please don't hesitate to contact us at <?php echo $support_email; ?></p>
 <p>Thank you,<br><?php echo HTML::chars($app_name); ?></p>

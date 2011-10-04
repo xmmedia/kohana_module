c5e48f791a37d7ce59c6381fb71faf7dc7316223
Darryl Hein <dhein@xmmedia.com>
2011-10-04 00:45:39 -0600

    changes related to the new menu structure

---
+++ classes/controller/xm/base.php
@@ -0,0 +1,25 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+class Controller_XM_Base extends Controller_cl4_Base {
+	/**
+	* Sets up the template script var, add's modernizr, jquery, jquery ui, cl4.js and base.js if they are not already set
+	*
+	* @return  Controller_Base
+	*/
+	public function add_template_js() {
+		if (empty($this->template->modernizr_path)) $this->template->modernizr_path = 'js/modernizr.min.js';
+
+		if (empty($this->template->scripts)) $this->template->scripts = array();
+		// add jquery js (for all pages, other js relies on it, so it has to be included first)
+		if ( ! isset($this->template->scripts['jquery'])) $this->template->scripts['jquery'] = '//ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js';
+		if ( ! isset($this->template->scripts['jquery_ui'])) $this->template->scripts['jquery_ui'] = '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js';
+		if ( ! isset($this->template->scripts['cl4'])) $this->template->scripts['cl4'] = 'cl4/js/cl4.js';
+		if ( ! isset($this->template->scripts['cl4_ajax'])) $this->template->scripts['cl4_ajax'] = 'cl4/js/ajax.js';
+		if ( ! isset($this->template->scripts['jquery_outside'])) $this->template->scripts['jquery_outside'] = 'js/jquery.outside.min.js';
+		if ( ! isset($this->template->scripts['base'])) $this->template->scripts['base'] = 'js/base.js';
+
+		if (empty($this->template->on_load_js)) $this->template->on_load_js = '';
+
+		return $this;
+	} // function add_template_js
+}
--- views/cl4/base/header.php
+++ views/cl4/base/header.php
@@ -1,58 +1,51 @@
 <header>
-	<div class="page_top_wrapper">
-		<div class="page_top">
-			<div class="language_options"><?php if ( ! empty($language_options)) { ?><span class="language_label"><?php echo __('Language: '); ?></span><?php echo $language_options; ?><?php } ?></div>
-			<div class="clear"></div>
-			<div class="page_top_logo"><a href="<?php echo URL::base(); ?>"><?php echo HTML::chars(SHORT_NAME . ' v' . APP_VERSION); if (isset($pageTitle) && trim($pageTitle) != '') echo ' - ' . HTML::chars($pageTitle); ?></a></div>
-			<div class="clear"></div>
-		</div>
+	<div class="top_nav_wrapper">
+		<nav>
+			<ul class="left">
+				<li class="home"><?php echo HTML::anchor('', __('Home')); ?></li>
+				<li class="aboutus has_subnav"><?php echo HTML::anchor(Route::get('pages')->uri(array('page' => 'aboutus')), __('About Us') . '<span class="more"></span>'); ?>
+					<ul class="sub_nav">
+						<li class="ourpeople"><?php echo HTML::anchor(Route::get('pages_section')->uri(array('section' => 'aboutus', 'page' => 'ourpeople')), __('Our People')); ?></li>
+					</ul>
+				</li>
+				<?php if ($logged_in) { ?>
+				<?php if (Auth::instance()->allowed('cl4admin') || Auth::instance()->allowed('cl4admin/model_create') || Auth::instance()->allowed('dbchange/index')) { ?>
+				<li class="dbadmin has_subnav"><?php echo HTML::anchor(Route::get('cl4admin')->uri(), __('DB Admin') . '<span class="more"></span>'); ?>
+					<ul class="sub_nav">
+						<?php if (Auth::instance()->allowed('useradmin/index')) { ?>
+						<li class="useradmin"><?php echo HTML::anchor(Route::get('useradmin')->uri(), __('Users Admin')); ?></li>
+						<?php } ?>
+						<?php if (Auth::instance()->allowed('cl4admin/model_create')) { ?>
+						<li class="modelcreate"><?php echo HTML::anchor(Route::get('cl4admin')->uri(array('model' => 'a', 'action' => 'model_create')), __('Model Create')); ?></li>
+						<?php } ?>
+						<?php if (Auth::instance()->allowed('dbchange/index')) { ?>
+						<li class="dbchange"><?php echo HTML::anchor(Route::get('dbchange')->uri(), __('DB Change')); ?></li>
+						<?php } ?>
+					</ul>
+				</li>
+				<?php } ?>
+			</ul>
+			<ul class="right">
+				<li class="right user has_subnav"><a href="" title="My Account"><?php echo HTML::chars($user->first_name . ' ' . $user->last_name); ?><span class="more"></span></a>
+					<ul class="sub_nav right">
+						<?php if (Auth::instance()->allowed('account/profile')) { ?>
+						<li class="account"><?php echo HTML::anchor(Route::get('account')->uri(array('action' => 'profile')), HTML::chars(__('My Account'))); ?></li>
+						<?php } ?>
+						<li class="logout"><?php echo HTML::anchor(Route::get('login')->uri(array('action' => 'logout')), HTML::chars(__('Logout'))); ?></li>
+					</ul>
+				</li>
+				<?php } else { ?>
+			</ul>
+			<ul class="right">
+				<li class="login"><?php echo HTML::anchor(Route::get('login')->uri(), HTML::chars(__('Login'))); ?></li>
+				<?php } ?>
+			</ul>
+		</nav>
+	</div>
+
+	<div class="page_top">
+		<div class="page_top_logo"><a href="<?php echo URL::base(); ?>"><?php echo HTML::chars(SHORT_NAME . ' v' . APP_VERSION); if (isset($pageTitle) && trim($pageTitle) != '') echo ' - ' . HTML::chars($pageTitle); ?></a></div>
+		<div class="clear"></div>
 	</div>
 	<div class="clear"></div>
-	<div class="nav_wrapper_repeat">
-		<div class="nav_wrapper">
-			<?php // displays on the right hand side, includes login, logout and profile link ?>
-			<nav class="basic_nav main_nav logged_in_nav">
-				<ul>
-					<?php if ($logged_in) { ?>
-					<li class="logout"><?php echo HTML::anchor(Route::get('login')->uri(array('action' => 'logout')), '<img src="/images/nav/logout.gif" width="12" height="12" alt="' . HTML::chars(__('Logout')) . '"> ' . HTML::chars(__('Logout'))); ?></li>
-					<li class="nav_divider"></li>
-					<li class="account"><?php echo HTML::anchor(Route::get('account')->uri(array('action' => 'profile')), '<img src="/images/nav/my_account.gif" width="10" height="13" alt="' . HTML::chars(__('My Account')) . '"> ' . HTML::chars(__('My Account'))); ?></li>
-					<?php } else { ?>
-					<li class="login"><?php echo HTML::anchor(Route::get('login')->uri(), '<img src="/images/nav/logout.gif" width="10" height="13" alt="' . HTML::chars(__('Login')) . '"> ' . HTML::chars(__('Login'))); ?></li>
-					<?php } // if logged in ?>
-					<li class="nav_divider"></li>
-				</ul>
-				<?php if ($logged_in) { ?>
-				<div class="page_top_user_info"><span class="login_in_as">Logged in as</span> <?php echo HTML::chars($user->first_name . ' ' . $user->last_name); ?></div>
-				<?php } // if logged in ?>
-			</nav>
-			<nav class="basic_nav main_nav">
-				<ul>
-					<li class="home"><?php echo HTML::anchor('', __('Home')); ?></li>
-					<li class="nav_divider"></li>
-					<li class="aboutus"><?php echo HTML::anchor(Route::get('pages')->uri(array('page' => 'aboutus')), __('About Us')); ?>
-						<ul class="sub_nav">
-							<li class="ourpeople"><?php echo HTML::anchor(Route::get('pages_section')->uri(array('section' => 'aboutus', 'page' => 'ourpeople')), __('Our People')); ?></li>
-						</ul>
-					</li>
-					<?php if ($logged_in && Auth::instance()->allowed('cl4admin')) { ?>
-					<li class="nav_divider"></li>
-					<li class="dbadmin"><?php echo HTML::anchor(Route::get('cl4admin')->uri(), __('DB Admin')); ?>
-						<ul class="sub_nav">
-							<?php if (Auth::instance()->allowed('useradmin/index')) { ?>
-							<li class="useradmin"><?php echo HTML::anchor(Route::get('useradmin')->uri(), __('User Admin')); ?></li>
-							<?php } ?>
-							<?php if (Auth::instance()->allowed('cl4admin/model_create')) { ?>
-							<li class="modelcreate"><?php echo HTML::anchor(Route::get('cl4admin')->uri(array('model' => 'a', 'action' => 'model_create')), __('Model Create')); ?></li>
-							<?php } ?>
-							<?php if (Auth::instance()->allowed('dbchange/index')) { ?>
-							<li class="dbchange"><?php echo HTML::anchor(Route::get('dbchange')->uri(), __('DB Change')); ?></li>
-							<?php } ?>
-						</ul>
-					</li>
-					<?php } ?>
-				</ul>
-			</nav>
-		</div>
-	</div>
 </header>

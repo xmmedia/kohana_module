7227a2945813d113a081f83eec7285635100d452
Darryl Hein <dhein@xmmedia.com>
2012-05-16 23:09:14 -0600

    updating jquery to 1.7.2

--- classes/controller/xm/base.php
+++ classes/controller/xm/base.php
@@ -11,7 +11,7 @@ class Controller_XM_Base extends Controller_cl4_Base {
 
 		if (empty($this->template->scripts)) $this->template->scripts = array();
 		// add jquery js (for all pages, other js relies on it, so it has to be included first)
-		if ( ! isset($this->template->scripts['jquery'])) $this->template->scripts['jquery'] = '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+		if ( ! isset($this->template->scripts['jquery'])) $this->template->scripts['jquery'] = '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
 		if ( ! isset($this->template->scripts['jquery_ui'])) $this->template->scripts['jquery_ui'] = '//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js';
 		if ( ! isset($this->template->scripts['cl4'])) $this->template->scripts['cl4'] = 'cl4/js/cl4.js';
 		if ( ! isset($this->template->scripts['cl4_ajax'])) $this->template->scripts['cl4_ajax'] = 'cl4/js/ajax.js';

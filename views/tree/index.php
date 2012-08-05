da1a7a45c76d51112ae3f75fa6b5608ea9d62e16
Darryl Hein <dhein@xmmedia.com>
2012-08-04 21:34:55 -0600

    creating the base layout for the tree edit

--- views/tree/index.php
+++ views/tree/index.php
@@ -1,26 +1,50 @@
+<p><a href="" class="cl4_add add_item">&nbsp;Add Item</a></p>
+
 <ul class="tree">
-	<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div>
+	<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element 1</div>
+		<div class="links">
+			<a href="" class="cl4_edit edit_item" title="Edit Item"></a>
+			<a href="" class="cl4_delete delete_item" title="Delete Item"></a>
+			<a href="" class="cl4_add add_sub_item" title="Add Sub Item"></a>
+		</div>
 		<ul>
-			<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div>
+			<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element 2</div><div class="links"><a href="" class="cl4_edit"></a></div>
 				<ul>
-					<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div>
+					<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element 3</div><div class="links"><a href="" class="cl4_edit"></a></div>
 						<ul>
-							<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div>
+							<li><a href="" class="no_expand"></a><div class="name">Tree Element 4a</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+							<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element 4</div><div class="links"><a href="" class="cl4_edit"></a></div>
 								<ul>
-									<li><a href="" class="no_expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+									<li><a href="" class="no_expand"></a><div class="name">Tree Element 5</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+								</ul>
+							</li>
+							<li><a href="" class="no_expand"></a><div class="name">Tree Element 4b</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+						</ul>
+					</li>
+					<li><a href="" class="no_expand"></a><div class="name">Tree Element 10</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+				</ul>
+			</li>
+			<li><a href="" class="no_expand"></a><div class="name">Tree Element 10</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+			<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element 6</div><div class="links"><a href="" class="cl4_edit"></a><a href="" class="cl4_delete"></a></div>
+				<ul>
+					<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element 7</div><div class="links"><a href="" class="cl4_edit"></a></div>
+						<ul>
+							<li class="has_children"><a href="" class="expand"></a><div class="name">Tree Element 8</div><div class="links"><a href="" class="cl4_edit"></a></div>
+								<ul>
+									<li><a href="" class="no_expand"></a><div class="name">Tree Element 9</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
 								</ul>
 							</li>
 						</ul>
 					</li>
 				</ul>
 			</li>
-			<li><a href="" class="no_expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
-			<li><a href="" class="no_expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
-			<li><a href="" class="no_expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+			<li><a href="" class="no_expand"></a><div class="name">Tree Element 10</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+			<li><a href="" class="no_expand"></a><div class="name">Tree Element 11</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
 		</ul>
 	</li>
-	<li><a href="" class="no_expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
-	<li><a href="" class="expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
-	<li><a href="" class="no_expand"></a><div class="name">Tree Element</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
-
+	<li><a href="" class="no_expand"></a><div class="name">Tree Element 12</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+	<li><a href="" class="no_expand"></a><div class="name">Tree Element 13</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
+	<li><a href="" class="no_expand"></a><div class="name">Tree Element 14</div><div class="links"><a href="" class="cl4_edit"></a></div></li>
 </ul>
+
+<div id="tree_dialog"></div>

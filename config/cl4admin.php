f23c08430fa98d3c07f5cbe693db757cbcc61d95
Darryl Hein <dhein@xmmedia.com>
2012-09-14 14:38:16 -0600

    adding the content admin tables to the cl4 admin list
    shouldn't show up unless the tables/models exist

---
+++ config/cl4admin.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'model_list' => array(
+		'content' => 'Content',
+		'content_history' => 'Content - History',
+		'content_page' => 'Content - Page',
+	),
+);

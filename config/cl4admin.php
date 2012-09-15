01c7a418eaafbaabfbf9684830235d4d5710e9ae
Darryl Hein <dhein@xmmedia.com>
2012-09-15 12:14:21 -0600

    adding the tables to the model list for the admin

--- config/cl4admin.php
+++ config/cl4admin.php
@@ -2,6 +2,28 @@
 
 return array(
 	'model_list' => array(
+		// cart tables
+		'cart_discount' => 'Cart - Discounts',
+		'cart_discount_product' => 'Cart - Discount - Products',
+		'cart_gift_card' => 'Cart - Gift Cards',
+		'cart_gift_card_use' => 'Cart - Gift Card - Uses',
+		'cart_order' => 'Cart - Orders',
+		'cart_order_additional_charge' => 'Cart - Order - Additional Charge',
+		'cart_order_discount' => 'Cart - Order - Discount',
+		'cart_order_log' => 'Cart - Order - Log',
+		'cart_order_payment' => 'Cart - Order - Payments',
+		'cart_order_product' => 'Cart - Order - Products',
+		'cart_order_product_property' => 'Cart - Order - Product - Properties',
+		'cart_order_shipping' => 'Cart - Order - Shippings',
+		'cart_order_tax' => 'Cart - Order - Taxes',
+		'cart_product' => 'Cart - Products',
+		'cart_product_property' => 'Cart - Product - Properties',
+		'cart_property' => 'Cart - Properties',
+		'cart_shipping' => 'Cart - Shipping',
+		'cart_shipping_location' => 'Cart - Shipping - Locations',
+		'cart_tax' => 'Cart - Taxes',
+
+		// content admin tables
 		'content' => 'Content',
 		'content_history' => 'Content - History',
 		'content_page' => 'Content - Page',

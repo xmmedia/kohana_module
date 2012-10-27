5a2148b2b990c5b28c11f95428b69fcabb22d5b5
Darryl Hein <dhein@xmmedia.com>
2012-10-26 23:41:30 -0600

    adding the correct validation message for valid email

--- messages/validation.php
+++ messages/validation.php
@@ -3,4 +3,7 @@
 return array(
 	'valid_date'         => 'A valid date must be entered for :field',
 	'greater_than'         => 'A value greater than :param2 must be entered for :field',
+
+	// this is a correction for the kohana validation message (a -> an)
+	'email'         => ':field must be an email address',
 );

4934b4143376d84a66abbc3a8b0a942d733c6b95
Darryl Hein <dhein@xmmedia.com>
2012-11-07 12:08:28 -0700

    setting the default to require auth on admin controllers
    removing the duplicate template setting

--- classes/Controller/XM/Admin.php
+++ classes/Controller/XM/Admin.php
@@ -4,7 +4,13 @@
  * Public controller for admin pages.
  */
 class Controller_XM_Admin extends Controller_Base {
-	public $template = 'base/template';
+	/**
+	 * Controls access for the whole controller.
+	 * If the entire controller REQUIRES that the user be logged in, set this to TRUE.
+	 * If some or all of the controller DOES NOT need to be logged in, set to this FALSE; to control which actions require authentication or a specific permission, us the $secure_actions array.
+	 * By default, all Admin Controllers are auth required = 5
+	 */
+	public $auth_required = FALSE;
 
 	/**
 	 * Called before the action.

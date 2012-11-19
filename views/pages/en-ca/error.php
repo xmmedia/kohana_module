9121e9b27de5f661e6ddc29e8e0f5211648cd307
Darryl Hein <dhein@xmmedia.com>
2012-11-19 12:56:17 -0700

    moving the error view to cl4base
    mo

--- views/pages/en-ca/error.php
+++
@@ -1,8 +0,0 @@
-<div class="container">
-	<div class="row">
-		<div class="twelvecol">
-			<h1><?php echo HTML::chars($title); ?></h1>
-			<p><?php echo HTML::chars($message); ?></p>
-		</div>
-	</div>
-</div>

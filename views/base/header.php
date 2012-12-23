d8d3439dffad5fa5ca5d02ce39ee7792b7b88f19
Darryl Hein <dhein@xmmedia.com>
2012-12-22 21:35:47 -0700

    some minor cleanup in views/base/header.php
    adding comments
    using CL4::is_dev() instead of checking Kohana::$environment directly

--- views/base/header.php
+++ views/base/header.php
@@ -4,7 +4,7 @@
 			<ul class="left">
 				<li class="home"><?php echo HTML::anchor('', __('Home')); ?></li>
 				<?php if ($logged_in) { ?>
-				<?php if (Auth::instance()->allowed('content_admin') || Auth::instance()->allowed('user_admin/index') || Auth::instance()->allowed('cl4admin') || (Auth::instance()->allowed('userguide') && Kohana::$environment == Kohana::DEVELOPMENT) || Auth::instance()->allowed('cl4admin/model_create') || Auth::instance()->allowed('db_change/index')) { ?>
+				<?php if (Auth::instance()->allowed('content_admin') || Auth::instance()->allowed('user_admin/index') || Auth::instance()->allowed('cl4admin') || (CL4::is_dev() && Auth::instance()->allowed('userguide')) || Auth::instance()->allowed('cl4admin/model_create') || Auth::instance()->allowed('db_change/index')) { ?>
 				<li class="dbadmin has_subnav"><?php echo HTML::anchor(Route::get('cl4admin')->uri(), __('DB Admin') . '<span class="more"></span>'); ?>
 					<ul class="sub_nav">
 						<?php if (Auth::instance()->allowed('user_admin/index')) { ?>
@@ -19,7 +19,7 @@
 						<?php if (Auth::instance()->allowed('cl4admin')) { ?>
 						<li class="cl4admin"><?php echo HTML::anchor(Route::get('cl4admin')->uri(), __('DB Admin')); ?></li>
 						<?php } ?>
-						<?php if (Auth::instance()->allowed('userguide') && Kohana::$environment == Kohana::DEVELOPMENT) { ?>
+						<?php if (CL4::is_dev() && Auth::instance()->allowed('userguide')) { ?>
 						<li class="user_guide"><?php echo HTML::anchor(Route::get('docs/guide')->uri(), __('Kohana User Guide')); ?></li>
 						<li class="user_guide_api"><?php echo HTML::anchor(Route::get('docs/api')->uri(), __('Kohana API Browser')); ?></li>
 						<?php } ?>
@@ -31,7 +31,7 @@
 						<?php } ?>
 					</ul>
 				</li>
-				<?php } ?>
+				<?php } // if db admin perm ?>
 			</ul>
 			<ul class="right">
 				<li class="right user has_subnav"><a href="" title="My Account"><?php echo HTML::chars($user->first_name . ' ' . $user->last_name); ?><span class="more"></span></a>
@@ -42,11 +42,11 @@
 						<li class="logout"><?php echo HTML::anchor(Route::get('login')->uri(array('action' => 'logout')), HTML::chars(__('Logout'))); ?></li>
 					</ul>
 				</li>
-				<?php } else { ?>
+				<?php } else { // if logged in ?>
 			</ul>
 			<ul class="right">
 				<li class="login"><?php echo HTML::anchor(Route::get('login')->uri(), HTML::chars(__('Login'))); ?></li>
-				<?php } ?>
+				<?php } // if logged in ?>
 			</ul>
 		</nav>
 	</div>

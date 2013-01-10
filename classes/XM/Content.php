e1d41073b7468be7a1c6f11c147f17f464f0f941
Darryl Hein <dhein@xmmedia.com>
2013-01-10 01:01:09 -0700

    adding merge tags to the content admin

--- classes/XM/Content.php
+++ classes/XM/Content.php
@@ -34,10 +34,10 @@ class XM_Content {
 				}
 
 				if (Arr::get($_REQUEST, 'draft', FALSE) && $content_item->has_draft()) {
-					$html .= $content_item->get_draft()->content;
+					$html .= Content::content($content_item->get_draft());
 					$showing_draft = TRUE;
 				} else {
-					$html .= $content_item->content;
+					$html .= Content::content($content_item);
 					$showing_draft = FALSE;
 				}
 
@@ -64,7 +64,7 @@ class XM_Content {
 					$html .= '</div>';
 				}
 			} else {
-				$html = $content_item->content;
+				$html = Content::content($content_item);
 			}
 
 			return $html;
@@ -72,6 +72,54 @@ class XM_Content {
 			throw new Kohana_Exception('The content item :code cannot be found', array(':code' => $code));
 		}
 	} // function display
+
+	/**
+	 * Returns the content for the content item, doing any additional processing that's needed.
+	 * Right now, this check for merge tags. The allowed formats are:
+	 *
+	 *     *|MERGE_TAG|*
+	 *     *|MERGE_TAG:value|*
+	 *     *|MERGE_TAG:value|other data|*
+	 *
+	 * If multiple of the same (exact other than case) merge tags are found, the first return will be used for all of them.
+	 * The merge tag will be converted into a method name, ie, `*|EMAIL_HIDE|*` will call `Content::_merge_email_hide()`.
+	 * The full content after all previous replacements have been completed and the merge tag
+	 * minus the deliminators will be sent to the merge method.
+	 * The merge method should only return what replaces the merge tag.
+	 *
+	 * @param  Model_Content_Item  $content_item  The Content Item model.
+	 * @return  string
+	 */
+	protected static function content($content_item) {
+		// check to see if we have an opening and closing merge "tag" within the content
+		if (strpos($content_item->content, '*|') !== FALSE && strpos($content_item->content, '|*') !== FALSE) {
+			$content = $content_item->content;
+
+			// find all the merge tags within the content
+			$regex = '/\*\|(.+?)\|\*/misu';
+			$tag_count = preg_match_all($regex, $content, $merge_tags);
+			if ($tag_count > 0) {
+				// loop through the merge tags
+				foreach ($merge_tags[1] as $merge_tag) {
+					$pipe_pos = strpos($merge_tag, '|');
+					$colon_pos = strpos($merge_tag, ':');
+					$merge_tag_name_end = $colon_pos < $pipe_pos ? $colon_pos : $pipe_pos;
+					$merge_tag_name = substr($merge_tag, 0, $merge_tag_name_end);
+
+					// run the method for the merge tag
+					$method = '_merge_' . strtolower($merge_tag_name);
+					$return = Content::$method($content, $merge_tag);
+
+					$content = UTF8::str_ireplace('*|' . $merge_tag . '|*', $return, $content);
+				}
+			}
+
+			return $content;
+
+		} else {
+			return $content_item->content;
+		}
+	} // function content
 
 	/**
 	 * Returns TRUE if the user is allowed to edit the content item.

ed10c7da636fb2f53d51aacc5f280d947a5dc9ea
Darryl Hein <dhein@xmmedia.com>
2013-08-31 23:56:37 -0600

    fixing the contact messages
    psr-0 problems

--- messages/Contact/contact.php
+++ messages/Contact/contact.php
@@ -1,4 +1,4 @@
 <?php defined('SYSPATH') or die ('No direct script access.');
 
-$msg_files = Kohana::find_file('messages', 'Contact', NULL, TRUE);
+$msg_files = Kohana::find_file('messages', 'contact', NULL, TRUE);
 return Kohana::load($msg_files[0]);

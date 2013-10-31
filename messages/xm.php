ef11d7570879ad555ab26d6579db514329d77ef4
Darryl Hein <dhein@xmmedia.com>
2013-10-30 20:45:18 -0600

    removing cl4 from a few message files

--- messages/xm.php
+++ messages/xm.php
@@ -1,13 +1,13 @@
 <?php defined('SYSPATH') or die ('No direct script access.');
 
 /**
-* Messages used throughout the cl4 modules
+* Messages used throughout the xm modules
 */
 
 return array(
-	'cl4_unknown_html' => '<span class="cl4_unknown">unknown</span>',
-	'cl4_not_set_html' => '<span class="cl4_not_set">not set</span>',
+	'xm_unknown_html' => '<span class="xm_unknown">unknown</span>',
+	'xm_not_set_html' => '<span class="xm_not_set">not set</span>',
 
-	// displayed when the exception handler fails (exception is caught inside the cl4 exception handler)
+	// displayed when the exception handler fails (exception is caught inside the xm exception handler)
 	'error_on_page' => 'There was an error while generating the page. Please try again later.',
 );

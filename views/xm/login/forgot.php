4f2fd61e9b8f1ee99898ff547feeeaf9823214b8
Darryl Hein <dhein@xmmedia.com>
2013-11-11 00:38:19 -0700

    simplifying the login & forgot password forms

--- views/xm/login/forgot.php
+++ views/xm/login/forgot.php
@@ -1,7 +1,16 @@
 <div class="login_box">
 	<h1>Reset Your Password</h1>
 
-	<?php echo Form::open(Request::current()); ?>
-	<p><label for="reset_username">To start, enter your email address:</label> <?php echo Form::input('reset_username', NULL, array('autofocus', 'size' => 30, 'maxlength' => 100, 'id' => 'reset_username')); ?></p>
-	<?php echo Form::submit(NULL, 'Reset Password'), Form::close(); ?>
+	<?php echo Form::open(Request::current()),
+		Form::hidden('forgot_submitted', 1); ?>
+
+	<div class="field">
+		<label for="reset_username">To start, enter your email address:</label>
+		<?php echo Form::input('reset_username', NULL, array('autofocus', 'size' => 30, 'maxlength' => 100, 'id' => 'reset_username')); ?>
+	</div>
+
+	<?php echo Form::submit(NULL, 'Reset Password'),
+		Form::close(); ?>
+
+	<div class="go_link"><?php echo HTML::anchor(Route::get('login')->uri(), 'Login') ?></div>
 </div>
--- views/xm/login/forgot_reset.php
+++ views/xm/login/forgot_reset.php
@@ -2,7 +2,7 @@
 	<h1>Enter a New Password</h1>
 
 	<?php echo Form::open(Request::current()),
-		Form::hidden('new_password_entered', 1),
+		Form::hidden('new_password_submitted', 1),
 		Form::hidden('username', $username),
 		Form::hidden('reset_token', $reset_token); ?>
 
@@ -15,5 +15,8 @@
 		<?php echo Form::password('password_confirm', NULL, array('size' => 20, 'maxlength' => 255, 'id' => 'password_confirm')); ?>
 	</div>
 
-	<?php echo Form::submit(NULL, 'Submit'), Form::close(); ?>
+	<?php echo Form::submit(NULL, 'Submit'),
+		Form::close(); ?>
+
+	<div class="go_link"><?php echo HTML::anchor(Route::get('login')->uri(), 'Login') ?></div>
 </div>

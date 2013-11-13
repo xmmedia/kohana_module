15e949f0e76a052e7efb977065b6b3c4115b966e
Darryl Hein <dhein@xmmedia.com>
2013-11-13 16:01:55 -0700

    tweaks to the user admin emails
    changing font for password to courier; adding escaping; "tabbing" in copy & paste url

--- views/user_admin/account_update_email.php
+++ views/user_admin/account_update_email.php
@@ -1,7 +1,7 @@
 <p>Your <?php echo HTML::chars($app_name); ?> account has been updated. Your new login information is as follows:</p>
-<p>Username: <?php echo $username; ?><br>
-Password: <?php echo ($password === FALSE ? '<em>Your current password</em>' : $password); ?></p>
+<p>Username: <?php echo HTML::chars($username); ?><br>
+	Password: <?php echo ($password === FALSE ? '<em>Your current password</em>' : '<span style="font-family: Courier, "Courier New", monospace;">' . HTML::chars($password) . '</span>'); ?></p>
 <p>To login, <?php echo HTML::anchor($url, 'click here', array('target' => '_blank'), TRUE); ?> or copy and paste the following link into your browser:</p>
-<p><?php echo HTML::chars($url); ?></p>
+<p style="padding-left: 3em;"><?php echo HTML::chars($url); ?></p>
 <p>If you have any problems, please don't hesitate to contact us at <?php echo $support_email; ?></p>
 <p>Thank you,<br><?php echo HTML::chars($app_name); ?></p>

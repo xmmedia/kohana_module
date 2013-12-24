db4ec9829dc8ab9f4c5330c0b79ba669669a84cd
Darryl Hein <dhein@xmmedia.com>
2013-12-24 15:33:41 -0700

    re-adding the selected validation msg

--- messages/validation.php
+++ messages/validation.php
@@ -4,6 +4,7 @@ return array(
 	'valid_date'   => 'A valid date must be entered for :field',
 	'greater_than' => 'A value greater than :param2 must be entered for :field',
 	'not_in_array' => 'Choose a different value for :field. The current value is not allowed',
+	'selected'     => 'A value must be selected for :field',
 
 	// this is a correction for the kohana validation message (a -> an)
 	'email'        => ':field must be an email address',

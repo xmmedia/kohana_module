68a86ccb78535c92408f426c61d62809368f6310
Darryl Hein <dhein@xmmedia.com>
2014-01-02 00:27:25 -0700

    completing v1 of the parse all error logs

--- classes/XM/Task/Error/Log/Parse/All.php
+++ classes/XM/Task/Error/Log/Parse/All.php
@@ -1,36 +1,41 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
- * Error log parse.
+ * Error log parse all.
  *
  * @package    XM
  * @category   Errors
  * @author     XM Media Inc.
  * @copyright  (c) 2013 XM Media Inc.
  */
-class XM_Task_Error_Log_Parse extends Minion_Task {
+class XM_Task_Error_Log_Parse_All extends Minion_Task {
 	protected $_options = array(
-		'delete_file' => TRUE,
+		'delete_files' => TRUE,
 	);
 
 	/**
-	 * Error log parse.
+	 * Error log parse all.
 	 *
 	 * @return null
 	 */
 	protected function _execute(array $params) {
-		Error::parse($params['file']);
+		$file_count = 0;
 
-		if ($params['delete_file']) {
-			Error::delete_error_log_file($params['file']);
+		$error_log_files = Directory_Helper::list_files(Error::error_log_dir(), FALSE);
+		foreach ($error_log_files as $error_log_file) {
+			$error_log_file = str_replace(Error::error_log_dir() . DIRECTORY_SEPARATOR, '', $error_log_file);
+
+			Error::parse($error_log_file);
+
+			if ($params['delete_files']) {
+				Error::delete_error_log_file($error_log_file);
+			}
+
+			Minion_CLI::write('Parsed error log: ' . $error_log_file);
+
+			++ $file_count;
 		}
 
-		Minion_CLI::write('Parsing successful');
-		Minion_CLI::write('Parsed error log: ' . $params['file']);
+		Minion_CLI::write('Parsing successful. Parsed ' . $file_count . ' ' . Inflector::plural('file', $file_count) . '.');
 	}
-
-	/*public function build_validation(Validation $validation) {
-		return parent::build_validation($validation)
-			->rule('file', 'not_empty');
-	}*/
 }

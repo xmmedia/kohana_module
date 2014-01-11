d7d020fc10c50b3a24188d3f1b5b2f05a47e1cd4
Darryl Hein <dhein@xmmedia.com>
2014-01-11 15:05:29 -0700

    adding the select multiple field type

--- classes/XM/ORM/SelectMultiple.php
+++ classes/XM/ORM/SelectMultiple.php
@@ -1,15 +1,24 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
-class XM_ORM_Select extends ORM_FieldType {
+class XM_ORM_SelectMultiple extends ORM_Select {
 	public static function edit($column_name, $html_name, $selected, array $attributes = NULL, array $options = array(), ORM $orm_model = NULL) {
 		$source = $orm_model->get_source_data($column_name);
+
+		if ( ! array_key_exists('multiple', $attributes)) {
+			$attributes['multiple'] = TRUE;
+			if (substr($html_name, -2, 2) != '[]') {
+				$html_name .= '[]';
+			}
+		}
+
+		$selected = explode(',', $selected);
 
 		// the default options are everything off, except for the None
 		// this will ensure there is the ability to set something to none or not set
 		$default_options = array(
 			'select_one' => FALSE,
 			'select_all' => FALSE,
-			'select_none' => TRUE,
+			'select_none' => FALSE,
 		);
 		$options = array_merge($default_options, $options);
 
@@ -18,6 +27,10 @@ class XM_ORM_Select extends ORM_FieldType {
 
 	public static function save($post, $column_name, array $options = array(), ORM $orm_model = NULL) {
 		$value = Arr::get($post, $column_name);
+
+		if (! empty($value)) {
+			$value = implode(',', (array) $value);
+		}
 
 		if ($value !== NULL || $options['is_nullable']) {
 			$orm_model->$column_name = ($value == 'none' || $value == 'all' || $value == '' ? 0 : $value);
@@ -48,7 +61,7 @@ class XM_ORM_Select extends ORM_FieldType {
 		}
 
 		return Form::select($html_name, $source, $selected, $attributes, $options);
-	} // function
+	}
 
 	/**
 	* Also used by ORM_Radios & ORM_YesNo & ORM_Gender (through ORM_Radios)
@@ -69,41 +82,42 @@ class XM_ORM_Select extends ORM_FieldType {
 		// none is in the array so search for anything not set (0 or NULL)
 		} else if (in_array('none', $value)) {
 			$methods = array(
-				// open a bracket
-				array(
-					'name' => $options['search_type'] . '_open',
-				),
 				// add clause to check for anything set to 0
 				array(
 					'name' => 'where',
-					'args' => array($sql_table_name . $column_name, '=', 0),
+					'args' => array($sql_table_name . $column_name, '=', DB::expr("''")),
 				),
-				// add clause to check for anything set to NULL
-				array(
-					'name' => 'or_where',
-					'args' => array($sql_table_name . $column_name, '=', NULL),
-				),
-				// close the bracket
-				array(
-					'name' => $options['search_type'] . '_close',
-				)
 			);
 
-		// we have some values, so do an IN
 		} else {
-			$methods = array(
-				array(
-					// don't need to include key name because it is where and set within ORM::set_search()
-					'args' => array($sql_table_name . $column_name, 'IN', $value),
-				)
-			);
+			foreach ($value as $_value) {
+				$methods[] = array(
+					'name' => 'or_where',
+					'args' => array(DB::expr('FIND_IN_SET(' . Database::instance()->quote($_value) . ', ' . Database::instance()->quote_column($sql_table_name . '.' . $column_name) . ')'), '>', 0),
+				);
+			}
 		}
 
 		return $methods;
-	} // function
+	}
 
 	public static function view($value, $column_name, ORM $orm_model = NULL, array $options = array(), $source = NULL) {
-		$found_value = Arr::get($source, $value);
+		$found_value = NULL;
+		$val_strings = array();
+
+		$values = explode(',', $value);
+		if ( ! empty($values) && $values[0] !== '') {
+			foreach ($values as $val) {
+				$found_value = Arr::get($source, $val);
+				if ( ! empty($found_value)) {
+					$val_strings[] = $found_value;
+				}
+			}
+
+			if ( ! empty($val_strings)) {
+				$found_value = implode(', ', $val_strings);
+			}
+		}
 
 		if ($found_value !== NULL) {
 			return $found_value;
@@ -113,7 +127,7 @@ class XM_ORM_Select extends ORM_FieldType {
 	}
 
 	public static function view_html($value, $column_name, ORM $orm_model = NULL, array $options = array(), $source = NULL) {
-		$found_value = ORM_Select::view($value, $column_name, $orm_model, $options, $source);
+		$found_value = ORM_SelectMultiple::view($value, $column_name, $orm_model, $options, $source);
 
 		if ($found_value !== NULL && $found_value !== 0) {
 			return ORM_Select::prepare_html(__($found_value), $options['nbsp']);

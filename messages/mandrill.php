53d11c3cb02b42bcf5e857a65c6aec7d016c9e12
Darryl Hein <dhein@xmmedia.com>
2014-02-28 16:26:08 -0700

    adding mandrill api

---
+++ messages/mandrill.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die ('No direct script access.');
+
+return array(
+	'rejected' => array(
+		'hard-bounce' => 'The email has been permanently rejected.',
+		'soft-bounce' => 'The email has been temporarily rejected. Please try again later.',
+		'spam' => 'The email has been marked as spam and cannot be sent.',
+		'unsub' => 'The email address has unsubscribed from the list.',
+		'custom' => 'The email address has been manually blocked.',
+	),
+);

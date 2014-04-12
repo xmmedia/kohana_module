75e6b3896dbe319a746c9baadc8a7880ab37b117
Darryl Hein <dhein@xmmedia.com>
2014-04-12 14:33:25 -0600

    html formatting

--- views/xm/account/profile.php
+++ views/xm/account/profile.php
@@ -3,14 +3,14 @@
 <div class="account_form">
 	<?php echo $form_open; ?>
 
-	<?php foreach ($columns as $column_name) : ?>
-		<div class="field"><?php echo $user->get_field_layout($column_name); ?></div>
-		<?php if ($column_name == 'username') : ?>
-			<div class="field"><label>Password</label><?php echo HTML::anchor($password_uri, 'Change password'); ?></div>
-		<?php endif ?>
-	<?php endforeach ?>
+		<?php foreach ($columns as $column_name) : ?>
+			<div class="field"><?php echo $user->get_field_layout($column_name); ?></div>
+			<?php if ($column_name == 'username') : ?>
+				<div class="field"><label>Password</label><?php echo HTML::anchor($password_uri, 'Change password'); ?></div>
+			<?php endif ?>
+		<?php endforeach ?>
 
-	<div class="buttons"><?php echo Form::button(NULL, 'Save'), HTML::anchor($default_uri, 'Reset'); ?></div>
+		<div class="buttons"><?php echo Form::button(NULL, 'Save'), HTML::anchor($default_uri, 'Reset'); ?></div>
 
 	</form>
 </div>

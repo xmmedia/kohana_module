0bb842c3bc75c24051181b28322711c99b89ccf2
Darryl Hein <dhein@xmmedia.com>
2014-05-08 15:26:53 -0600

    a couple forgot password email cleanup items

--- views/xm/login/forgot_confirm_email.php
+++ views/xm/login/forgot_confirm_email.php
@@ -1,7 +1,7 @@
 <p>This is a notice to confirm that your password has been changed on the <?php echo HTML::chars($app_name); ?>.</p>
 <p>If the you did not change your password, please contact the administrator immediately at: <?php echo HTML::mailto($admin_email); ?></p>
 
-<p>To login, <?php echo HTML::anchor($url, 'click here', array('target' => '_blank'), TRUE); ?> or copy and paste the following link into your browser:</p>
-<p style="padding-left: 3em;"><?php echo HTML::chars(URL::site($url, TRUE)); ?></p>
+<p><?php echo HTML::anchor($url, 'Login to ' . HTML::chars($app_name), array('target' => '_blank'), TRUE); ?></p>
 
-<p>Thank you,<br><?php echo HTML::chars($app_name); ?></p>
+<p>Thank you,<br>
+	<?php echo HTML::chars($app_name); ?></p>

30c174744e4a8d316887de4ddf0b5cce84af864d
Darryl Hein <dhein@xmmedia.com>
2014-05-08 16:56:45 -0600

    adjusting the formatting of the emails

--- views/xm/login/forgot_confirm_email.php
+++ views/xm/login/forgot_confirm_email.php
@@ -1,7 +1,7 @@
 <p>This is a notice to confirm that your password has been changed on the <?php echo HTML::chars($app_name); ?>.</p>
 <p>If the you did not change your password, please contact the administrator immediately at: <?php echo HTML::mailto($admin_email); ?></p>
 
-<p><?php echo HTML::anchor($url, 'Login to ' . HTML::chars($app_name), array('target' => '_blank'), TRUE); ?></p>
+<p style="padding-left: 3em;"><?php echo HTML::anchor($url, 'Login to ' . HTML::chars($app_name), array('target' => '_blank'), TRUE); ?></p>
 
 <p>Thank you,<br>
 	<?php echo HTML::chars($app_name); ?></p>
